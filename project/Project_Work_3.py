79e68fbc76a44a99a44dd527084ca4a406a54bd8
Derinc <93079836+Derin6@users.noreply.github.com>
2023-12-17 18:22:48 +0100

    Update Project_Work_3.py

--- project/Project_Work_3.py
+++ project/Project_Work_3.py
@@ -23,11 +23,11 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 def extract(url):
     header = {
-  "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+  "User-Agent": 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:41.0) Gecko/20100101 Firefox/41.0',
+        
   "X-Requested-With": "XMLHttpRequest"
 }
     r = requests.get(url, headers=header)
-    
     return r
     
 
