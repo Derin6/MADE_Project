404c109a9f12abeedccbcf7b1e51bf36fbda2099
Derinc <93079836+Derin6@users.noreply.github.com>
2024-01-19 19:36:56 +0100

    Update exercise4.py

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -6,41 +6,10 @@
 
 import pandas as pd
 import sqlalchemy
-import urllib.request
 import requests
 import io 
-import ssl
-import urllib3
 import zipfile
 
-ssl._create_default_https_context = ssl._create_unverified_context
-
-
-# In[132]:
-
-
-class CustomHttpAdapter (requests.adapters.HTTPAdapter):
-    # "Transport adapter" that allows us to use custom ssl_context.
-
-    def __init__(self, ssl_context=None, **kwargs):
-        self.ssl_context = ssl_context
-        super().__init__(**kwargs)
-
-    def init_poolmanager(self, connections, maxsize, block=False):
-        self.poolmanager = urllib3.poolmanager.PoolManager(
-            num_pools=connections, maxsize=maxsize,
-            block=block, ssl_context=self.ssl_context)
-
-
-def get_legacy_session():
-    ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-    ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
-    session = requests.session()
-    session.mount('https://', CustomHttpAdapter(ctx))
-    return session
-
-
-# In[133]:
 
 
 def extract(url):
@@ -49,8 +18,9 @@ def extract(url):
         
   "X-Requested-With": "XMLHttpRequest"
 }
-    r = get_legacy_session().get(url, headers=header)
+    r = requests.get(url, headers=header)
     return r
+    
     
 
 
